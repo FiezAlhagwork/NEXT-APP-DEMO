179ddabbc3744c9a3a8a2144e6fdb52fd01b77ab
FiezAlhagwork <148772477+FiezAlhagwork@users.noreply.github.com>
2024-04-02 15:14:35 +0300

    Update PostDetals.jsx

--- app/components/PostDetals.jsx
+++ app/components/PostDetals.jsx
@@ -1,9 +1,4 @@
 const PostDetals = async ({title}) => {
-    await new Promise((resolve) => {
-        setTimeout(() => {
-          resolve();
-        }, 3000);
-      });
       const response = await fetch(
         `https://jsonplaceholder.typicode.com/posts/${title}`,
         {
